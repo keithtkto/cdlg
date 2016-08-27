b0b1928b5d4edd9bf9858852ee833d5170e41355
Keith To <keithtkto@gmail.com>
2016-08-27 12:53:05 -0700

    typed in all the credits

---
+++ client/helper/credits.js
@@ -0,0 +1,105 @@
+export const credits = [
+    {  band: 'Paramore',
+        album: 'forthcoming LP',
+        label: 'Atlantic/Fueled By Ramen',
+        year: 2017,
+        role: 'Engineer'
+    },
+    {  band: 'Cherry Glazerr',
+        album: 'Apocalipstick',
+        label: 'Secretly Canadian',
+        year: 2017,
+        role: 'Producer, Engineer, Mixer'
+    },
+    {  band: 'Jimmy Eat World',
+        album: 'forthcoming LP',
+        label: 'RCA',
+        year: 2017,
+        role: 'Engineer'
+    },
+    {  band: 'The Naked and Famous',
+        album: 'Simple Forms',
+        label: 'KLS/Somewhat Damaged',
+        year: 2016,
+        role: 'Engineer'
+    },
+    {  band: 'Ziggy Marley',
+        album: 'S/T',
+        label: 'Tough Gong Worldwide',
+        year: 2016,
+        role: 'Mixer, Engineer'
+    },
+    {  band: 'M83',
+        album: 'Junk',
+        label: 'Mute',
+        year: 2016,
+        role: 'Mixer, Engineer'
+    },
+    {  band: 'Bleached',
+        album: 'Welcome The Worms',
+        label: 'Dead Oceans',
+        year: 2016,
+        role: 'Producer, Engineer, Mixer'
+    },
+    {  band: 'Wild Belle',
+        album: 'Dreamland',
+        label: 'Columbia',
+        year: 2016,
+        role: 'Engineer, Guitar, Bass'
+    },
+    {  band: 'School of Seven Bells',
+        album: 'SVIIB',
+        label: 'Vagrant',
+        year: 2016,
+        role: 'Engineer'
+    },
+    {  band: 'YACHT',
+        album: 'I Thought the Future Would Be Cooler',
+        label: 'Downtown',
+        year: 2015,
+        role: 'Engineer'
+    },
+    {  band: 'WATERS',
+        album: 'What’s Real',
+        label: 'Vagrant',
+        year: 2015,
+        role: 'Producer, Engineer, Mixer, Keys'
+    },
+    {  band: 'Young the Giant',
+        album: 'Mind Over Matter',
+        label: 'Atlantic/Fueled By Ramen',
+        year: 2015,
+        role: 'Engineer'
+    },
+    {  band: 'The Colourist',
+        album: 'Self-Titled',
+        label: 'Universal/Republic',
+        year: 2014,
+        role: 'Producer, Engineer, Keys'
+    },
+    {  band: 'Tegan and Sara',
+        album: 'Heartthrob',
+        label: 'Warner',
+        year: 2013,
+        role: 'Engineer'
+    },
+    {  band: 'Paramore',
+        album: 'Self-Titled',
+        label: 'Atlantic/Fueled By Ramen',
+        year: 2013,
+        role: 'Engineer, Percussion'
+    },
+    {  band: 'Neon Trees',
+        album: 'Picture Show',
+        label: 'Mercury',
+        year: 2012,
+        role: 'Engineer, Percussion'
+    },
+    {  band: 'Neon Trees',
+        album: 'Habits',
+        label: 'Mercury',
+        year: 2010,
+        role: 'Engineer, Mixer'
+    },
+
+]
